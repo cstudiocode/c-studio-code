62198cdf6c5c76a3986ec8847ca91446fd5f4065
fordimalanda <fordimalanda7@gmail.com>
2025-03-30 08:09:06 +0200

    Nouvelle fonctionnalité

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication  # Import manquant
 from compiler.compiler import Compiler
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.compiler = Compiler()  # Initialise le compilateur
+        self.compiler = Compiler()
         self._setup_ui()
+        self.setWindowTitle("C Studio Code")  # Titre de la fenêtre
+        self.resize(800, 600)  # Taille par défaut
+
+    def _setup_ui(self):
+        """Initialise l'interface utilisateur"""
+        # ... (votre code d'interface existant)
 
     def _compile_code(self):
         code = self.editor.text()
@@ -14,3 +22,9 @@ class MainWindow(QMainWindow):
             self._show_output("Compilation réussie !")
         else:
             self._show_output(f"Erreur :\n{errors}")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)  # Crée l'application Qt
+    window = MainWindow()         # Crée la fenêtre principale
+    window.show()                 # Affiche la fenêtre
+    sys.exit(app.exec_())         # Lance la boucle d'événements
